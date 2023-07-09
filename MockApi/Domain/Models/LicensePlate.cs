2a4d785cc6e7ba8087631c4a15dc9758eff1077d
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-07-09 21:43:00 +0300

    add license plate model class

---
+++ MockApi/Domain/Models/LicensePlate.cs
@@ -0,0 +1,16 @@
+﻿namespace MockApi.Domain.Models
+{
+    public class LicensePlate
+    {
+        public string Series { get; }
+        public int Number { get; }
+        public int Region { get; }
+
+        public LicensePlate(string series, int number, int region)
+        {
+            Series = series;
+            Number = number;
+            Region = region;
+        }
+    }
+}
