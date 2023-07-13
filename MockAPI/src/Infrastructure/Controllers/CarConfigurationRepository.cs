aed929fef2be0a5df0d8ee93e4641953c7cfd633
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-07-14 00:00:44 +0300

    ad car config repository

---
+++ MockAPI/src/Infrastructure/Controllers/CarConfigurationRepository.cs
@@ -0,0 +1,23 @@
+﻿using Core.Application.Interfaces;
+using Core.Domain.Models;
+
+namespace Core.Infrastructure.Controllers
+{
+    public class CarConfigurationRepository : ICarConfigurationRepository
+    {
+        void ICarConfigurationRepository.Add(CarConfiguration configuration)
+        {
+            throw new NotImplementedException();
+        }
+
+        CarConfiguration ICarConfigurationRepository.GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICarConfigurationRepository.RemoveByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
